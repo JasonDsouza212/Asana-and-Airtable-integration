3faaeb13514ad6717b2bb7d8fae8d055f04d854d
Your Name <you@example.com>
2023-08-23 11:50:04 +0530

    new console message

--- server.js
+++ server.js
@@ -4,53 +4,44 @@ const crypto = require("crypto");
 const app = express();
 app.use(express.json());
 
-let secret = "";
+let secret = ""; // This is where you'll store the secret during the handshake
 
-app.get('/',(req,res)=>{
-  console.log("connected to ngrok ")
-})
+app.get('/', (req, res) => {
+  console.log("Connected to ngrok");
+  res.sendStatus(200);
+});
 
 app.post("/receiveWebhook", (req, res) => {
-  console.log(req)
-  if (req.headers["x-hook-secret"]) {
-    console.log("This is a new webhook");
-    secret = req.headers["x-hook-secret"];
+  console.log("Webhook received "+ req.body);
 
-    res.setHeader("X-Hook-Secret", secret);
-    res.sendStatus(200);
-  } else if (req.headers["x-hook-signature"]) {
-    const computedSignature = crypto
-      .createHmac("SHA256", secret)
-      .update(JSON.stringify(req.body))
-      .digest("hex");
-
-    if (
-      !crypto.timingSafeEqual(
-        Buffer.from(req.headers["x-hook-signature"]),
-        Buffer.from(computedSignature)
-      )
-    ) {
-      // Fail
-      res.sendStatus(401);
-    } else {
-      // Success
+  if (req.headers["x-hook-signature"]) {
+    // During the handshake, check if it's a new webhook and store the secret
+    if (req.headers["x-hook-signature"] === "new-webhook") {
+      secret = req.body["secret"];
+      console.log("Received new webhook secret:", secret);
       res.sendStatus(200);
-      console.log(`Events on ${Date()}:`);
-      console.log(req.body.events);
+    } else {
+      const computedSignature = crypto
+        .createHmac("sha256", secret)
+        .update(JSON.stringify(req.body))
+        .digest("hex");
+
+      if (req.headers["x-hook-signature"] === computedSignature) {
+        console.log("Webhook signature verified");
+        res.sendStatus(200);
+        console.log(`Events on ${Date()}:`);
+        console.log(req.body.events);
+      } else {
+        console.log("Webhook signature verification failed");
+        res.sendStatus(401);
+      }
     }
   } else {
-    console.error("Something went wrong!");
+    console.error("No x-hook-signature header found in request");
+    res.sendStatus(400);
   }
 });
 
 app.listen(8080, () => {
   console.log(`Server started on port 8080`);
 });
-
-
-
-
-
-
-
-
