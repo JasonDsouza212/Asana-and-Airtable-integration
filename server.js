4c2f9fba3b99203b4ab54a72db4954e969ec0dc9
Your Name <you@example.com>
2023-08-23 12:01:45 +0530

    initial commit

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const crypto = require("crypto");
+const cors = require("cors"); 
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 let secret = ""; // This is where you'll store the secret during the handshake
 
